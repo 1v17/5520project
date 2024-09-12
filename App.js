f2c6d2262374c33fba275cc62e51780b43a0bc88
1v17 <166570730+1v17@users.noreply.github.com>
2024-09-12 11:29:54 -0700

    change app's name

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const appName = 'Strawberry Fields';
+  const appName = 'Penny Lane';
   return (
     <View style={styles.container}>
       <Text>Welcome to {appName}</Text>
