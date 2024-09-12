ead98f45fd7a76e0931feddfedce124208a1fd29
1v17 <166570730+1v17@users.noreply.github.com>
2024-09-12 12:03:46 -0700

    change header component and add to app

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/Header';
 
 export default function App() {
   const appName = 'Penny Lane';
   return (
     <View style={styles.container}>
-      <Text>Welcome to {appName}</Text>
+
       <StatusBar style="auto" />
+      <Header name={appName}/>
     </View>
   );
 }
--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,13 @@
-return (
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Header({name}) {
+//   console.log(props)
+  return (
     <View>
-        {/* Your content here */}
+      <Text>Welcome to {name}</Text>
     </View>
-);
+  )
+}
+
+const styles = StyleSheet.create({})
