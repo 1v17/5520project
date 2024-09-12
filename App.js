171fa0b17db80269b82c6bf3ae2270bb9a306556
1v17 <166570730+1v17@users.noreply.github.com>
2024-09-12 14:40:12 -0700

    Delete empty lines

--- App.js
+++ App.js
@@ -13,8 +13,6 @@ export default function App() {
       <StatusBar style="auto" />
       <Header name={appName}/>
       <Input autoFocus={true}/>
-      {/* <Text>{text}</Text> */}
-
     </View>
   );
 }
