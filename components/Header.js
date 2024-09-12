84a74ce5e9135f29f884b3597de224f82ab44d9c
1v17 <166570730+1v17@users.noreply.github.com>
2024-09-12 12:10:54 -0700

    let header component receive children

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function Header({name}) {
+export default function Header({name, children}) {
 //   console.log(props)
   return (
     <View>
