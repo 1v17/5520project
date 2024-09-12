1fe1c6123173d87506fb04950cf6ac166b528edb
1v17 <166570730+1v17@users.noreply.github.com>
2024-09-12 14:28:08 -0700

    make counter invisible when losing focus

--- components/Input.js
+++ components/Input.js
@@ -5,29 +5,28 @@ import { useState, useRef, useEffect } from 'react';
 
 export default function Input({autoFocus}) {
     const [text, setText] = useState("");
-    const inputRef = useRef(null);
     const [characterCount, setCharacterCount] = useState(0);
+    const [counterVisible, setCounterVisible] = useState(true);
 
     useEffect(() => {
-        if (autoFocus) {
-            inputRef.current.focus();
-        }
         setCharacterCount(text.length);
-    }, [autoFocus, text]);
+    }, [text]);
 
     return (
         <>
             <TextInput
-                ref={inputRef}
-                placeholder="Input sth here"
+                placeholder="Input words here"
                 keyboardType='default'
                 style={{borderBottomColor: "blue", borderBottomWidth: 2}}
                 value={text}
                 onChangeText={function (changedText) {
                     setText(changedText);
                 }}
+                autoFocus={autoFocus}
+                blurOnSubmit={true}
+                onBlur={function () {setCounterVisible(false)}}
             />
-            {text.length > 0 && autoFocus && <Text>{characterCount}</Text>}
+            {text.length > 0 && counterVisible && <Text >{characterCount}</Text>}
         </>
     )
 }
