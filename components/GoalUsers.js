393e5a68c22689c88c03fb295b08e78a9cf2d3b1
1v17 <166570730+1v17@users.noreply.github.com>
2024-10-24 12:23:28 -0700

    read data from firebase first before connected to API

--- components/GoalUsers.js
+++ components/GoalUsers.js
@@ -1,26 +1,35 @@
 import { View, Text, FlatList } from "react-native";
 import { useEffect, useState } from "react";
 import React from "react";
-import { writeToDB } from "../firebase/FirebaseHelper";
+import { writeToDB, readAllDocs } from "../firebase/FirebaseHelper";
 
 const GoalUsers = ({ goalId }) => {
+  const collectionName = `goals/${goalId}/users`;
+
   useEffect(() => {
     async function fetchData() {
       // can't mark the useEffect callback as async,
       // so create an async function inside
       try {
-        const response = await fetch(
-          "https://jsonplaceholder.typicode.com/users"
-        );
-        if (!response.ok) {
-          throw new Error(`HTTP error happend with status ${response.status}`);
+        const dataFromDB = await readAllDocs(collectionName);
+        if (dataFromDB.length) {
+          setUsers(dataFromDB.map((user) => user.name));
+        } else {
+          const response = await fetch(
+            "https://jsonplaceholder.typicode.com/users"
+          );
+          if (!response.ok) {
+            throw new Error(
+              `HTTP error happend with status ${response.status}`
+            );
+          }
+          const usersData = await response.json();
+
+          usersData.forEach((user) => {
+            writeToDB(user, collectionName);
+          });
+          setUsers(usersData.map((user) => user.name));
         }
-        const usersData = await response.json();
-        const collectionName = `goals/${goalId}/users`;
-        usersData.forEach((user) => {
-          writeToDB(user, collectionName);
-        });
-        setUsers(usersData.map((user) => user.name));
       } catch (err) {
         console.log("Fetch user data", err);
       }
--- firebase/FirebaseHelper.js
+++ firebase/FirebaseHelper.js
@@ -1,4 +1,11 @@
-import { collection, addDoc, deleteDoc, doc, getDocs, updateDoc } from "firebase/firestore"; 
+import {
+  collection,
+  addDoc,
+  deleteDoc,
+  doc,
+  getDocs,
+  updateDoc,
+} from "firebase/firestore";
 import { database } from "./FirebaseSetup";
 
 export async function writeToDB(data, collectionName) {
@@ -6,39 +13,50 @@ export async function writeToDB(data, collectionName) {
     await addDoc(collection(database, collectionName), data);
     // console.log("Document written with ID: ", docRef.id);
     // using async/await to handle the promise returned by addDoc when calling the function
-  }
-  catch (err) {
-   console.log("Write to DB ", err);
+  } catch (err) {
+    console.log("Write to DB ", err);
   }
 }
 
 export async function deleteFromDB(id, collectionName) {
-  try { 
+  try {
     await deleteDoc(doc(database, collectionName, id));
     // console.log("Document deleted with ID: ", id);
-  }
-  catch (err) {
+  } catch (err) {
     console.log("Delete from DB", err);
   }
 }
 
 export async function deleteAllFromDB(collectionName) {
-  try { 
+  try {
     const querySnapshot = await getDocs(collection(database, collectionName));
     querySnapshot.forEach((snapshotDoc) => {
-    deleteFromDB(snapshotDoc.id, collectionName);
+      deleteFromDB(snapshotDoc.id, collectionName);
     });
-  }
-  catch (err) {
-    console.log("Delete all from DB", err)
+  } catch (err) {
+    console.log("Delete all from DB", err);
   }
 }
 
-export async function updateDB(id, data, collectionName) {
-  try { // you can use setDoc() and merge: true as well, if the document does not exist, it will be created
-    await updateDoc(doc(database, collectionName, id), data); 
+export async function readAllDocs(collectionName) {
+  try {
+    const querySnapshot = await getDocs(collection(database, collectionName));
+    let newArray = [];
+    querySnapshot.forEach((doc) => {
+      newArray.push({ ...doc.data(), id: doc.id });
+    });
+    return newArray;
+  } catch (err) {
+    console.log("Read all docs", err);
   }
-  catch (err) {
+  
+}
+
+export async function updateDB(id, data, collectionName) {
+  try {
+    // you can use setDoc() and merge: true as well, if the document does not exist, it will be created
+    await updateDoc(doc(database, collectionName, id), data);
+  } catch (err) {
     console.log("Update DB ", err);
   }
 }
